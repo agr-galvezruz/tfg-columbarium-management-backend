0a431eb216519d84e1c3e8832dcc948f5ff8fe03
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-19 16:51:55 +0200

    Urn store and update request

---
+++ app/Http/Requests/V1/UpdateUrnRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUrnRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+      $method = $this->method();
+      if ($method == 'PUT') {
+        return [
+          'internalCode' => ['required', 'unique:urns,internal_code,'. $this->id .',id,niche_id,'. $this->niche_id],
+          'status' => Rule::in(['OCCUPIED', 'RESERVED', 'AVAILABLE', 'DISABLED']),
+          'nicheId' => ['required']
+        ];
+      }
+    }
+
+    protected function prepareForValidation() {
+      $this->merge([
+        'internal_code' => $this->internalCode,
+        'niche_id' => $this->nicheId
+      ]);
+    }
+}
